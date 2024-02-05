2b981252d7ff5de9779ce0fcafe8649cd6586316
Eli Fuchsman <efuchsman@gmail.com>
2024-02-05 16:13:13 -0700

    WIP troubleshooting api gateway

--- silence_of_the_lambdas.go
+++ silence_of_the_lambdas.go
@@ -15,6 +15,10 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+func init() {
+	log.SetFormatter(&log.TextFormatter{})
+}
+
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	err := godotenv.Load()
 	if err != nil {
@@ -37,11 +41,12 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 	sOTLClient := silence.NewSilenceOfTheLambdasClient(db)
 	silenceHandler := handlers.NewHandler(sOTLClient)
 
+	log.Printf("Received %s request for path: %s", request.HTTPMethod, request.Path)
+
 	switch request.HTTPMethod {
 	case "GET":
 		response, err := handleGetRequest(request, silenceHandler, db)
 		if err != nil {
-			// Handle the error, for example, log it
 			log.Printf("Error handling GET request: %v", err)
 			return events.APIGatewayProxyResponse{
 				StatusCode: 500,
@@ -59,7 +64,14 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 
 func handleGetRequest(request events.APIGatewayProxyRequest, handler *handlers.Handler, db *ddb.SilenceOfTheLambsDB) (*events.APIGatewayProxyResponse, error) {
 	switch request.Path {
+	case "/":
+		log.Println("Handling root path request.")
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 200,
+			Body:       "Hello, World!",
+		}, nil
 	case "/killers/full_name":
+		log.Println("Handling /killers/full_name path request.")
 		tableName := os.Getenv("DYNAMODB_TABLE_1_NAME")
 		response := handler.GetKiller(request, tableName, db)
 		return response, nil
