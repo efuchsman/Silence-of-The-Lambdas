e7fb7f54a2d829c2910860815e400386c6990864
dbunch-klaviyo <134417365+dbunch-klaviyo@users.noreply.github.com>
2024-02-07 14:49:16 -0700

    Comments silence_of_the_lambdas.go

--- silence_of_the_lambdas.go
+++ silence_of_the_lambdas.go
@@ -3,30 +3,49 @@ package main
 import (
 	"context"
 	"strings"
-
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/efuchsman/Silence-of-The-Lambdas/handlers"
+	"github.com/joho/godotenv"
+	
 	silence "github.com/efuchsman/Silence-of-The-Lambdas/internal/silence_of_the_lambdas"
 	ddb "github.com/efuchsman/Silence-of-The-Lambdas/internal/silence_of_the_lambs_db"
-	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 )
 
-func init() {
+// main should be the core function and initialize critical dependencies, process flags, init logging etc. 
+// Additionally the file should be called `main.go`
+func main() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetOutput(os.Stdout)
+
+
+
+
+
+	lambda.Start(Handler)
+	log.StandardLogger().Exit(0) // Odd to see a hard exit for a logger
 }
 
+// You're passing this function to lamda.Start. It should be initialized here if it's a critical dependency.
+
+type LambdaHandler struct {
+	db *ddb.SilenceOfTheLambsDB
+	// TODO
+}
+
+// func (h *LambdaHandler) handleGetRequest(...) {...}
+
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// A lot of this should be moved into main, especially when calling log.Fail.
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// These should all be handled in main()
 	awsAccessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
 	awsSecretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
 	awsRegion := os.Getenv("AWS_REGION")
@@ -49,6 +68,7 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 		"httpMethod": request.HTTPMethod,
 	}).Info("Request details")
 
+	// This can be passed directly to the handling function. It's hidden where it currently is.
 	if request.HTTPMethod == "GET" {
 		response, err := handleGetRequest(request, silenceHandler, db)
 		if err != nil {
@@ -70,6 +90,7 @@ func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 	}, nil
 }
 
+// More reason to separate the above function - lots of unecessaryly hidden functionality and overall flow patterns. It looks like the above should be a new handler struct, which is passed into the lambda.
 func handleGetRequest(request events.APIGatewayProxyRequest, handler *handlers.Handler, db *ddb.SilenceOfTheLambsDB) (*events.APIGatewayProxyResponse, error) {
 	cleanedPath := strings.TrimSpace(request.Path)
 	log.WithFields(log.Fields{
@@ -107,9 +128,4 @@ func handleGetRequest(request events.APIGatewayProxyRequest, handler *handlers.H
 			Body:       "Not Found",
 		}, nil
 	}
-}
-
-func main() {
-	lambda.Start(Handler)
-	log.StandardLogger().Exit(0)
 }
